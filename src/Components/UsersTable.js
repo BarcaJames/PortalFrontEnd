0242337e120dc8afd8b95817be0124a5da2fcc9a
Jamie James <49459617+BarcaJames@users.noreply.github.com>
2021-11-22 01:08:46 -0500

    Added table to display all users

---
+++ src/Components/UsersTable.js
@@ -0,0 +1,118 @@
+import { useDispatch } from "react-redux";
+import { Table, Image, Badge } from "react-bootstrap";
+import { toast } from "react-toastify";
+import {
+  useGetAllUsersQuery,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+} from "../store/UserApi";
+import { addSelectedUser } from "../features/user/selectedUserSlice";
+import { useState } from "react";
+import AddEditUserModal from "./AddEditUserModal";
+
+const UsersTable = ({ filterText, setShowModal }) => {
+  const dispatch = useDispatch();
+  const { data: allUsers, isSuccess } = useGetAllUsersQuery();
+  const [trigger] = useUpdateUserMutation();
+  const [deleteUserTrigger] = useDeleteUserMutation();
+  const [editUser, setEditUser] = useState(null);
+  const [showEditModal, setShowEditModal] = useState(false);
+
+  const handleCloseEditModal = () => setShowEditModal(false);
+
+  const handleDelete = (event, username) => {
+    event.stopPropagation();
+    deleteUserTrigger(username)
+      .unwrap()
+      .then((payload) => console.log("Fulfilled", payload))
+      .catch((err) => toast.error(err.data.message.toLowerCase()));
+  };
+
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Photo</th>
+            <th>User ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allUsers?.length && isSuccess
+            ? allUsers
+                .filter((user) =>
+                  user.username.toLowerCase().includes(filterText.toLowerCase())
+                )
+                .map((user) => (
+                  <tr
+                    key={user.userId}
+                    onClick={() => {
+                      setShowModal(true);
+                      dispatch(
+                        addSelectedUser({
+                          allUsers,
+                          selectedUserId: user.userId,
+                        })
+                      );
+                    }}
+                  >
+                    <td>
+                      <Image
+                        style={{ objectFit: "contain" }}
+                        width="60px"
+                        src={user.profileImageUrl}
+                        roundedCircle
+                        alt="profile-picture"
+                      />
+                    </td>
+                    <td>{user.userId}</td>
+                    <td>{user.firstName}</td>
+                    <td>{user.lastName}</td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                    <td>
+                      <Badge pill bg={user.active ? `primary` : "danger"}>
+                        {user.active ? `Active` : "Inactive"}
+                      </Badge>
+                    </td>
+                    <td>
+                      <i
+                        class="bi bi-pencil-square px-2"
+                        onClick={(event) => {
+                          event.stopPropagation();
+                          setEditUser(user);
+                          setShowEditModal(true);
+                        }}
+                      />
+                      <i
+                        class="bi bi-trash"
+                        onClick={(event) => handleDelete(event, user?.username)}
+                      />
+                    </td>
+                  </tr>
+                ))
+            : null}
+        </tbody>
+      </Table>
+
+      {editUser ? (
+        <AddEditUserModal
+          action={"Edit"}
+          title={"Edit "}
+          handleClose={handleCloseEditModal}
+          show={showEditModal}
+          editUser={editUser}
+          trigger={trigger}
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default UsersTable;
