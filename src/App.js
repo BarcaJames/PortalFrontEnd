3cefb4b0653ae7e3a9b71990fb01b206ebaad287
Jamie James <49459617+BarcaJames@users.noreply.github.com>
2021-11-22 00:49:32 -0500

    Added react router and config routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import "./App.css";
+import Login from "./Components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./Components/Register";
+import ManagementPortal from "./Components/ManagementPortal";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ToastContainer />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/user/management" element={<ManagementPortal />} />
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
