1671fc950281ef2ba8bec3ebca7040ebd402b3be
Jamie James <49459617+BarcaJames@users.noreply.github.com>
2021-11-22 00:52:22 -0500

    Added custom nav bar

---
+++ src/Components/CustomNavBar.js
@@ -0,0 +1,36 @@
+import { useSelector } from "react-redux";
+import { Nav } from "react-bootstrap";
+const CustomNavBar = ({ selectedNav, setSelectedNav }) => {
+  const user = useSelector((state) => state?.currentUser?.currentUser);
+  return (
+    <>
+      <Nav
+        className="bg-info p-2 my-2"
+        variant="pills"
+        activeKey={selectedNav}
+        onSelect={(selectedKey) => setSelectedNav(selectedKey)}
+      >
+        <Nav.Item>
+          <Nav.Link eventKey="Users">
+            <i class="bi bi-people-fill pe-1"></i>
+            Users
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="Settings">
+            <i class="bi bi-gear-fill pe-1"></i>
+            Settings
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="ms-auto">
+          <Nav.Link eventKey="Profile">
+            Welcome, {user?.firstName ? user?.firstName : null}
+            <i class="bi bi-person-fill ps-1"></i>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
+  );
+};
+
+export default CustomNavBar;
