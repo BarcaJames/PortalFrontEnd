ffafc00a7736e6e0f399f9276e1b2d05cdf5bead
Jamie James <49459617+BarcaJames@users.noreply.github.com>
2021-11-30 22:28:37 -0500

    Clean up code

--- src/hooks/use-getAuthInfo.js
+++ src/hooks/use-getAuthInfo.js
@@ -1,10 +1,8 @@
-import axios from "axios";
 import { decode } from "jsonwebtoken";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const useGetAuthInfo = () => {
-  const BASE_URL = "http://localhost:8080/user";
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   var current_time = new Date().getTime() / 1000;
@@ -36,42 +34,13 @@ const useGetAuthInfo = () => {
     navigate("/");
   };
 
-  // const getAuthoritiesFromToken = async () => {
-  //   let tokenValid = await isTokenValid();
-  //   if (tokenValid) {
-  //     return new Promise((resolve) => resolve(decodedToken.Authorities));
-  //   }
-  //   navigate("/");
-  // };
-
-  // const canUserDelete = async () => {
-  //   const authorities = await getAuthoritiesFromToken();
-  //   return authorities.includes("user:delete");
-  // };
-
   const hasAuthority = (authority) => {
     return user?.authorities.includes(authority);
-  };
-
-  const addUserToState = async () => {
-    let username = await getUsernameFromToken();
-    if (username) {
-      try {
-        const result = await axios.get(BASE_URL + `/find/${username}`);
-        console.log("result in addUserToState----> ", result);
-      } catch (error) {
-        localStorage.removeItem("token");
-        navigate("/");
-      }
-    }
-
-    navigate("/");
   };
 
   return {
     isTokenValid,
     getUsernameFromToken,
-    addUserToState,
     hasAuthority,
   };
 };
